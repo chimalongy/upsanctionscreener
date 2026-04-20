a25cf665f3e991b7d529dc31cfc28d8ea6f17af6
chimalongy <me.chimaobi@gmail.com>
2026-04-20 10:32:21 +0100

    Fresh start

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Upsanctionscreener.Controllers
+{
+    public class AuthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string email, string password)
+        {
+            if (!string.IsNullOrEmpty(email) && !string.IsNullOrEmpty(password))
+            {
+                return RedirectToAction("Index", "Dashboard");
+            }
+
+            ModelState.AddModelError("", "Invalid email or password.");
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            return RedirectToAction("Login", "Auth");
+        }
+    }
+}
---
+++ Views/Dashboard/List/PEPs.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Upsanctionscreener.Views.Dashboard.List
+{
+    public class PEPsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
