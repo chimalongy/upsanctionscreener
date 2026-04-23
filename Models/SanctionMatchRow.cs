e74835662d4cf42f2281509180b27144623927e9
chimalongy <me.chimaobi@gmail.com>
2026-04-23 13:58:26 +0100

    confirmed single search

--- Models/SanctionMatchRow.cs
+++ Models/SanctionMatchRow.cs
@@ -6,34 +6,20 @@ using System.Threading.Tasks;
 
 namespace Upsanctionscreener.Models
 {
-    public class SanctionMatchRow
+    public class SingleSearchSanctionMatchRow
     {
-        // BKSearchResult fields
-        public string EntryId { get; set; } = "";
-        public string MatchedName { get; set; } = "";
-        public double Similarity { get; set; }
-        public int EditDistance { get; set; }
+      public string similarity { get; set; }  
+      public SanctionEntry sanction_item { get; set; }
 
-        // SanctionEntry fields
-        public string ID { get; set; } = "";
-        public string SubjectType { get; set; } = "";
-        public string Source { get; set; } = "";
-        public string ReferenceNumber { get; set; } = "";
-        public string DateDesignated { get; set; } = "";
-        public string SanctionImposed { get; set; } = "";
-        public string Comments { get; set; } = "";
 
-        public string Names { get; set; } = "";
-        public string Addresses { get; set; } = "";
-        public string PhoneNumbers { get; set; } = "";
-        public string EmailAddresses { get; set; } = "";
-        public string Positions { get; set; } = "";
-        public string IdList { get; set; } = "";
 
-        public string? CallSign { get; set; }
-        public string? VesselType { get; set; }
-        public string? VesselFlag { get; set; }
-        public string? VesselOwner { get; set; }
-        public string? GrossRegisteredTonnage { get; set; }
+    }
+
+
+    public  class PepSearchSanctionMatchRow 
+    {
+        public string similarity { get; set; }
+        public PepEntry pep_item { get; set; }
+       
     }
 }
